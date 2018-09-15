6d900601f7044f24e2232ec7f2f845a7e9fcc579
James Rho <jrho1209@naver.com>
2018-09-15 13:32:29 -0400

    home

--- project2/ecommerce_test/src/containers/home/home-page.js
+++ project2/ecommerce_test/src/containers/home/home-page.js
@@ -1,5 +1,9 @@
 import * as React from 'react';
 import { Col, Container, Row } from 'reactstrap';
+import {TotalItems} from './compoents/TotalItems';
+import {TotalUsers} from './components/TotalUsers';
+import {TotalCompany} from './components/TotalCompany';
+import {TotalItemsTable} from './components/TotalItemsTable';
 
 export class HomePage extends React.PureComponent {
     render() {
@@ -11,6 +15,17 @@ export class HomePage extends React.PureComponent {
                     <Col md={12}>
                         <h3>Welcome to JM-Terrely</h3>
                     </Col>
+                </Row>
+
+                <Row>
+
+                    <TotalItems/>
+                    <TotalUsers/>
+                    <TotalCompany/>
+                </Row>
+
+                <Row>
+                    <TotalItemsTable/>
                 </Row>
                 
 
