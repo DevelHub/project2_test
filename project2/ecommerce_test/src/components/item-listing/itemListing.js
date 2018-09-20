6d4a631137d207497330d7322d62069640246b22
billyjb3 <billyjbrewer3@gmail.com>
2018-09-19 21:02:13 -0400

    structure change to item listing for wishlist. Creating wishlist page

--- project2/ecommerce_test/src/components/item-listing/itemListing.js
+++ project2/ecommerce_test/src/components/item-listing/itemListing.js
@@ -17,10 +17,11 @@ export class ItemListing extends React.Component
     {
         return (
             <div className="itemListing" onClick={this.listingClicked}>
-                <ItemTitle>{this.props.item.name}</ItemTitle>
+                {this.props.children}
+                {/* <ItemTitle>{this.props.item.name}</ItemTitle>
                 <ItemSubtitle>{this.props.item.subtitle}</ItemSubtitle>
                 <ItemImage src={this.props.item.image}/>
-                <ItemDescription>{this.props.item.description}</ItemDescription>
+                <ItemDescription>{this.props.item.description}</ItemDescription> */}
             </div>
         )
         
--- project2/ecommerce_test/src/containers/wishlist/wishlist.js
+++ project2/ecommerce_test/src/containers/wishlist/wishlist.js
@@ -1,13 +1,24 @@
 import * as React from 'react';
 import './wishlistStyles.css';
+import {ItemRow, ItemListing, ItemTitle, ItemSubtitle, ItemImage, ItemDescription} from '../../components/item-listing/index';
+
 
 export class Wishlist extends React.Component
 {
     render()
     {
+        const wishlistExample = [{
+            name: "I Wish I Had This",
+            company: ""
+        }]
         return (
             <div className="wishlistContainer">
+                <div className="wishlistItem">
+                    <div className="wishlistImage">
+                    </div>
 
+                    <div className="wishlistTitle"></div>
+                </div>
             </div>
         )
     }
